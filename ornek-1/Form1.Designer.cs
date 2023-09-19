06b845c35bf0d918b03175b128c39a99bed9b928
yazilimkodlama <145126274+yazilimkodlama@users.noreply.github.com>
2023-09-19 11:14:32 +0300

    Add files via upload
    2. label eklendi

--- ornek-1/Form1.Designer.cs
+++ ornek-1/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -36,17 +37,29 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.label1.Location = new System.Drawing.Point(170, 217);
+            this.label1.Location = new System.Drawing.Point(139, 114);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(510, 73);
             this.label1.TabIndex = 0;
             this.label1.Text = "Ahmet Cansever";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label2.Location = new System.Drawing.Point(139, 261);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(490, 73);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Hayri Koçmarlar";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -58,6 +71,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
